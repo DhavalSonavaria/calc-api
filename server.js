c8b11ed31a019abc163ae97fac499d93a08e2e7a
Dhaval Sonavaria <dhsonava@syr.edu>
2020-10-16 10:37:13 -0500

    Socket

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const socket = require('socket.io');
 const app = express();
 
 app.use(cors());
@@ -16,12 +16,21 @@ array.push = function (){
 }
 
 
+const server = app.listen(process.env.PORT,function(){
+	console.log('app is running on port ${process.env.PORT}');
+});
+
+app.use(express.static("public"));
+
+// Socket setup
+const io = socket(server);
+
+io.on("connection", function (socket) {
+  console.log("Made socket connection");
+});
+
 app.get('/',(req,res)=> {
 	res.send('this is working');
-})
-app.listen(process.env.PORT|| 8080,()=>{
-	console.log('app is running on port ${process.env.PORT}');
-
 })
 
 app.post('/calculate',(req,res)=>{
