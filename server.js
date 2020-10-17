c4b939e30dbc2ee54a0723a6ea9c6c0128ab8d2e
Dhaval Sonavaria <dhsonava@syr.edu>
2020-10-16 20:31:08 -0500

    socket

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const socket = require('socket.io');
-const app = express();
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,29 +13,10 @@ array.push = function (){
     return Array.prototype.push.apply(this,arguments);
 }
 
-
-const server = app.listen(process.env.PORT,function(){
-	console.log('app is running on port ${process.env.PORT}');
+io.on('connection',(socket)=>{
+	console.log('a user connected');
 });
 
-app.use(express.static("public"));
-
-// Socket setup
-const io = socket(server);
-
-io.on("connection", function (socket) {
-  console.log("Made socket connection");
+http.listen(process.env.PORT, () => {
+  console.log('listening on ${process.env.PORT}');
 });
-
-app.get('/',(req,res)=> {
-	res.send('this is working');
-})
-
-app.post('/calculate',(req,res)=>{
-	array.push(req.body.calculation)
-	res.send(JSON.stringify(array));
-})
-
-app.get('/logs',(req,res)=>{
-	res.send(JSON.stringify(array));
-})
