c92598434132951e429ef1ddac9368a619204845
Dhaval Sonavaria <dhsonava@syr.edu>
2020-10-16 20:33:26 -0500

    Socket.io

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.use(cors());
-app.use(bodyParser.json());
 
 var array = new Array()
 array.push = function (){
